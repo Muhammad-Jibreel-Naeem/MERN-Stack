d2bee793b212c2315d95bf5f9263217f53efe420
Muhammad Jibreel <news.naeem@gmail.com>
2025-01-28 15:19:10 +0530

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,24 @@
+<nav>
+
+<Link to="/">Home</Link>
+<Link to="/add-pet">Add Pet</Link>
+  
+</nav>
+
+<div style={
+backgroundSize: "cover",
+backgroundPosition: "center",
+height: "90vh",
+color: "#fff",
+display: "flex",
+flexDirection: "column",
+justifyContent: "center",
+alignItems: "center",
+textAlign: "center"
+}>
+  
+<h1>Welcome to Pet Heaven</h1>
+
+<p> Find your furry companion or give a home to pets in need. Explore available pets below </p>
+
+</div>
