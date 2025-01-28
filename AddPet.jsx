6ca5b8eea3f3807fd196d2585ca57bdcd3d89a9d
Muhammad Jibreel <news.naeem@gmail.com>
2025-01-28 15:57:17 +0530

    Update AddPet.jsx

--- AddPet.jsx
+++ AddPet.jsx
@@ -1,3 +1,30 @@
+const AddPet = () => {
+  const [pet, setPet] = useState({
+    name: "",
+    breed: "",
+    age: "",
+    gender: "",
+    location: "",
+    photo: null
+  })
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setPet({  ...pet, [e.target.name]: e.target.value })
+  }
+
+  const handleFileChange = async (e) => {
+    const file = e.target.files[0]
+    if (!file) {
+      console.error("No file selected")
+      return
+    }
+    const formData = new FormData()
+      formData.append("file", file)
+      formData.append("upload_preset", "pet_photos")
+      
+
 <form>
   <h1>Add a new pet</h1>
   <br/>
