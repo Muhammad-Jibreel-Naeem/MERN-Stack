12e5462339f4d65d0ab59b7c7f49358068b08c2b
Muhammad Jibreel <news.naeem@gmail.com>
2025-01-28 15:29:01 +0530

    Create AddPet.jsx

---
+++ AddPet.jsx
@@ -0,0 +1,24 @@
+<form>
+  <h1>Add a new pet</h1>
+  <br/>
+  Name <br/>
+  <input type="text"/>
+  
+  Breed <br/>
+  <input type="text"/>
+  
+  Age <br/>
+  <input type="text"/>
+  
+  Gender <br/>
+  <input type="text"/>
+
+  Location <br/>
+  <input type="text"/>
+  
+  Photo <br/>
+  <input type="file"/>
+
+<input type="submit"/>
+  
+</form>
