983d67b4a554d33f0aab3d72cc5d5dc444445d95
Muhammad Jibreel <news.naeem@gmail.com>
2025-01-30 14:39:39 +0530

    Update PetDetails.jsx
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- PetDetails.jsx
+++ PetDetails.jsx
@@ -25,6 +25,26 @@ const PetDetails = () => {
 if (!pet) return <div>Loading...</div>
                                
                                
+return (
+  <div>
+  <img src={pet.photo} alt={pet.name}/>
+    <h2>{pet.name}</h2>
+    <p>Breed: {pet.breed}</p>
+    <p>Age: {pet.age} years</p>
+    <p>Location: {pet.location}</p>
+    <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter Your Name..." name="adopterName" value={form.adopterName} onChange={(e) => setForm({ ...form, adopterName: e.target.value })} required/>
+      
+        <input type="email" placeholder="Enter Your Email..." name="email" value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} required/>
+
+        <input type="text" placeholder="Enter Your Phone Number..." name="phoneNumber" value={form.phoneNumber} onChange={(e) => setForm({ ...form, phoneNumber: e.target.value })} required/>
+
+        <input type="text" placeholder="Enter Your Name..." name="adopterName" value={form.adopterName} onChange={(e) => setForm({ ...form, adopterName: e.target.value })} required/>
+
+        <button type="submit" >Submit</button>
+      
+    </form>
+  </div>
                                
-                               
-                               )
+)
+export default PetDetails
